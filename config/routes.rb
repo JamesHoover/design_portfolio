5226c018ccddc4961d2def428fd56227ff1b75b2
James Hoover <jhoover@cequint.com>
2015-01-08 10:14:50 -0800

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ DesignPortfolio::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   #root 'application#hello'
-  root 'application#welcome'
+  root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/about' => 'pages#about'
+  get '/photography' => 'pages#photography'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
