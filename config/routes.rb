a031a91b1257c47997380748dfd6bc8b0947a7a7
James Macbook <jamesfhoover@gmail.com>
2016-04-13 20:46:31 -0700

    added photo page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ DesignPortfolio::Application.routes.draw do
   get '/projects/govote' => 'pages#govote'
   get '/projects/privacy' => 'pages#privacy'
   get '/projects/others' => 'pages#others'
+  get '/photography/' => 'pages#photography'
 
   # get '/work' => 'pages#projects'
   # get '/about' => 'pages#about'
