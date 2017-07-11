f9b418b4031b377a0e82cb6f01229925627739c7
James Macbook <jamesfhoover@gmail.com>
2017-07-10 21:41:02 -0700

    cleaned some stuff up, added some polish

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,9 +10,7 @@ $(document).ready(function(){
 
 // Radomize text/bg colors
 function randomize() {
-	var x = randomColors();
-	document.getElementById('colored__text').style.color = x;
-	document.getElementById('colored__link').style.boxShadow = "inset 0 -4px 0 " + x;
+	document.getElementById('colored__text').style.color = randomColors();
 	document.getElementById('colored__bg').style.backgroundColor = randomColors();
 }
 // random colors - taken from here:
@@ -20,25 +18,23 @@ function randomize() {
 function randomColors() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+
 //displays email on click
 function showEmail() {
-    var x = document.getElementById('showEmail');
-    if (x.style.display === 'inline-block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'inline-block';
-    }
-}
-//no socials, sucker
-function socialDisplay() {
-	document.getElementById('socialDisplay').innerHTML = 'Fred Flinstone';
+  var email = document.getElementById('showEmail');
+  if (email.style.display === 'inline-block') {
+      email.style.display = 'none';
+  } else {
+      email.style.display = 'inline-block';
+  }
 }
 
-function showFB() {
-    var x = document.getElementById('showFB');
-    if (x.style.display === 'inline-block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'inline-block';
-    }
+//display winnie
+function showName() {
+	var name = document.getElementById('showName');
+  if (name.style.display === 'inline-block') {
+      name.style.display = 'none';
+  } else {
+      name.style.display = 'inline-block';
+  }
 }
